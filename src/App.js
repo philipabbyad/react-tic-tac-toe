68810db2dac93eda9041d7bbc5c99d505d22113c
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2024-01-02 17:54:28 -0700

    Added Game component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,20 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
+
+function Board() {
   const [xIsNext, setXIsNext] = useState(true); 
   const [squares, setSquares] = useState(Array(9).fill(null));
 
